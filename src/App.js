f2126cd4f34e2c58f18ebf9d256eeefff9b29247
shrutilenka <shrutilenka1202@gmail.com>
2023-01-04 15:07:56 +0530

    ..check table component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 // import FiveA from "./components/5a";
-// import Table from "./components/table";
-import Five from "./components/5";
+import Table from "./components/table";
+// import Five from "./components/5";
 
 function App() {
   return (
     <div className="App">
-      {/* <Table /> */}
-      {/* <FiveA /> */}
-      <Five />
+      <Table />
+      {/* <FiveA />
+      <Five /> */}
     </div>
   );
 }
