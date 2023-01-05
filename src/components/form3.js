fcbaac0fcec2825012bb18e1de6c41c07056ffe9
shrutilenka <shrutilenka1202@gmail.com>
2023-01-05 22:23:39 +0530

    modified form3

--- src/components/form3.js
+++ src/components/form3.js
@@ -1,247 +1,344 @@
+import "./../styles/allStates.css";
+
 const Form3 = () => {
   return (
     <>
       {/* form 3 */}
-      <table>
+      <table className="form3">
         <tr>FORM - III</tr>
-        <tr>
-          <td>
-            <table>
-              <thead>
-                <tr>
-                  Profession Tax Registration <span>...</span>
-                </tr>
-                <tr>
-                  Certificate No. <span>...</span>
-                </tr>
-                <tr>B . S . T . R . C . No. if any</tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <p>
-                    <strong>Part - II</strong>
-                  </p>
-                  <p>
-                    THE MAHARASHTRA STATE TAX ON PROFESSIONS, TRADERS, CALLINGS
-                    AND EMPLOYMENTS ACT, 1975 AND RULE 11, 11-A, 11-B, 11-C
-                    0028, Other Taxes on Income and Expenditure-Taxes on
-                    Professions, Trades, Callings and Employments - Taxes on
-                    Employments
-                  </p>
-                </tr>
-
-                <tr>
-                  <td>Tax Amount</td>
-                  <td>
-                    Rs. <span>...</span>
-                  </td>
-                  <th>0.00</th>
-                </tr>
-                <tr>
-                  <td>Interest Amount</td>
-                  <td>
-                    Rs. <span>...</span>
-                  </td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>
-                    Less - Excess tax Paid, if any, in the Previous year / Qtr.
-                    / Month
-                  </td>
-                  <td>
-                    Rs. <span>...</span>
-                  </td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>Net Amount Payable</td>
-                  <td>
-                    Rs. <span>...</span>
-                  </td>
-                  <th>0.00</th>
-                </tr>
-                <tr>
-                  <th>Profession Tax Registration Certificate No.</th>
-                  <th>Period From</th>
-                  <th>Period To</th>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td>01-Jan-2021</td>
-                  <td>31-Jan-2021</td>
-                </tr>
-              </tbody>
-              <tfoot>
-                <tr>Name And Address</tr>
-                <tr>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div>
+            <tr>
+              <td>Profession Tax Registration Certificate No.</td>
+              <td colSpan={2}>..........</td>
+            </tr>
+            <tr>
+              <td>B . S . T . R . C . No. if any</td>
+              <td colSpan={2}>......</td>
+            </tr>
+            <tr>
+              <td colSpan={3}>
+                <p>
+                  <strong>Part - II</strong>
+                </p>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <p>Form - III</p>
+                  <p>RETURN-CUM-CHALAN</p>
+                  <p>For the Treasury</p>
+                </div>
+                <p>
+                  THE MAHARASHTRA STATE TAX ON PROFESSIONS, TRADERS, CALLINGS
+                  AND EMPLOYMENTS ACT, 1975 AND RULE 11, 11-A, 11-B, 11-C <br />
+                  0028, Other Taxes on Income and Expenditure-Taxes on
+                  Professions, Trades, Callings and Employments - Taxes on
+                  Employments
+                </p>
+              </td>
+            </tr>
+            <tr>
+              <td>Tax Amount</td>
+              <td>
+                Rs. <span>...</span>
+              </td>
+              <th>0.00</th>
+            </tr>
+            <tr>
+              <td>Interest Amount</td>
+              <td>
+                Rs. <span>...</span>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                Less - Excess tax Paid, if any, in the Previous year / Qtr. /
+                Month
+              </td>
+              <td>
+                Rs. <span>...</span>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Net Amount Payable</td>
+              <td>
+                Rs. <span>...</span>
+              </td>
+              <th>0.00</th>
+            </tr>
+            <tr>
+              <th>Profession Tax Registration Certificate No.</th>
+              <th>Period From</th>
+              <th>Period To</th>
+            </tr>
+            <tr>
+              <td></td>
+              <td>01-Jan-2021</td>
+              <td>31-Jan-2021</td>
+            </tr>
+            <tr>
+              <td colSpan={3}>
+                <p>Name And Address</p>
+                <p>
                   <strong>Lawcubator Technologies Pvt Ltd</strong>
-                </tr>
-                <tr>
+                </p>
+                <p>
                   758,3rd floor, 19th main, Dollar Scheme layout,HSR Layout 2nd
                   Sector, Bangalore, India-560102
-                </tr>
-                <tr>
+                </p>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={3}>
+                <p>
                   The above statements are true to the best of my knowledge and
                   belief
-                </tr>
-                <tr>
-                  <td>Date</td>
-                  <td>02-Jan-2023</td>
-                  <td>.....</td>
-                </tr>
-                <tr>
-                  <td>Place</td>
-                  <td></td>
-                  <td>Signature & Designation</td>
-                </tr>
-                <tr>For the treasury Use Only</tr>
-                <tr>
-                  <td>
-                    Received Rupees (in Words) <br /> <span> Rupees Zero</span>
-                  </td>
-                  <td>
-                    Rupees (in figures) <br /> <span> 0.00</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Date of Entry:</td>
-                  <td>Chalan No. :</td>
-                </tr>
-                <tr>
-                  <tr>
-                    <td>...</td>
-                    <td>...</td>
-                    <td>...</td>
-                  </tr>
-                  <tr>
-                    <td>Treasurer</td>
-                    <td>Accountant</td>
-                    <td>Treasury Officer / Agent or Manager</td>
-                  </tr>
-                </tr>
-              </tfoot>
-            </table>
-          </td>
-          <td>
-            <table>
-              <thead>
-                <tr>
-                  Profession Tax Registration <span>...</span>
-                </tr>
-                <tr>
-                  Certificate No. <span>...</span>
-                </tr>
-                <tr>B . S . T . R . C . No. if any</tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <p>
-                    <strong>Part - II</strong>
-                  </p>
-                  <p>
-                    THE MAHARASHTRA STATE TAX ON PROFESSIONS, TRADERS, CALLINGS
-                    AND EMPLOYMENTS ACT, 1975 AND RULE 11, 11-A, 11-B, 11-C
-                    0028, Other Taxes on Income and Expenditure-Taxes on
-                    Professions, Trades, Callings and Employments - Taxes on
-                    Employments
-                  </p>
-                </tr>
-
-                <tr>
-                  <td>Tax Amount</td>
-                  <td>
-                    Rs. <span>...</span>
-                  </td>
-                  <th>0.00</th>
-                </tr>
-                <tr>
-                  <td>Interest Amount</td>
-                  <td>
-                    Rs. <span>...</span>
-                  </td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>
-                    Less - Excess tax Paid, if any, in the Previous year / Qtr.
-                    / Month
-                  </td>
-                  <td>
-                    Rs. <span>...</span>
-                  </td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>Net Amount Payable</td>
-                  <td>
-                    Rs. <span>...</span>
-                  </td>
-                  <th>0.00</th>
-                </tr>
-                <tr>
-                  <th>Profession Tax Registration Certificate No.</th>
-                  <th>Period From</th>
-                  <th>Period To</th>
-                </tr>
-                <tr>
-                  <td></td>
-                  <td>01-Jan-2021</td>
-                  <td>31-Jan-2021</td>
-                </tr>
-              </tbody>
-              <tfoot>
-                <tr>Name And Address</tr>
-                <tr>
+                </p>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "baseline",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <div>
+                    <td>
+                      <td>Date</td>
+                      <td>02-Jan-2023</td>
+                    </td>
+                    <tr>
+                      <td>Place</td>
+                      <td>.....</td>
+                    </tr>
+                  </div>
+                  <div style={{ display: "flex", flexDirection: "column" }}>
+                    <p>....</p>
+                    <p>Signature & Designation</p>
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>For the treasury Use Only</tr>
+            <tr>
+              <td width={"50%"}>
+                Received Rupees (in Words) <br /> <span> Rupees Zero</span>
+              </td>
+              <td colSpan={2} width={"50%"}>
+                Rupees (in figures) <br /> <span> 0.00</span>
+              </td>
+            </tr>
+            <tr>
+              <td>Date of Entry:</td>
+              <td>Chalan No. :</td>
+            </tr>
+            <tr>
+              <td colSpan={3}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignContent: "center",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <p>....</p>
+                    <p>....</p>
+                    <p>....</p>
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <p>Treasurer</p>
+                    <p>Accountant</p>
+                    <p>Treasury Officer / Agent or Manager</p>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </div>
+          <div>
+            <tr>
+              <td>Profession Tax Registration Certificate No.</td>
+              <td colSpan={2}>..........</td>
+            </tr>
+            <tr>
+              <td>B . S . T . R . C . No. if any</td>
+              <td colSpan={2}>......</td>
+            </tr>
+            <tr>
+              <td colSpan={3}>
+                <p>
+                  <strong>Part - II</strong>
+                </p>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <p>Form - III</p>
+                  <p>RETURN-CUM-CHALAN</p>
+                  <p>For the Treasury</p>
+                </div>
+                <p>
+                  THE MAHARASHTRA STATE TAX ON PROFESSIONS, TRADERS, CALLINGS
+                  AND EMPLOYMENTS ACT, 1975 AND RULE 11, 11-A, 11-B, 11-C <br />
+                  0028, Other Taxes on Income and Expenditure-Taxes on
+                  Professions, Trades, Callings and Employments - Taxes on
+                  Employments
+                </p>
+              </td>
+            </tr>
+            <tr>
+              <td>Tax Amount</td>
+              <td>
+                Rs. <span>...</span>
+              </td>
+              <th>0.00</th>
+            </tr>
+            <tr>
+              <td>Interest Amount</td>
+              <td>
+                Rs. <span>...</span>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                Less - Excess tax Paid, if any, in the Previous year / Qtr. /
+                Month
+              </td>
+              <td>
+                Rs. <span>...</span>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>Net Amount Payable</td>
+              <td>
+                Rs. <span>...</span>
+              </td>
+              <th>0.00</th>
+            </tr>
+            <tr>
+              <th>Profession Tax Registration Certificate No.</th>
+              <th>Period From</th>
+              <th>Period To</th>
+            </tr>
+            <tr>
+              <td></td>
+              <td>01-Jan-2021</td>
+              <td>31-Jan-2021</td>
+            </tr>
+            <tr>
+              <td colSpan={3}>
+                <p>Name And Address</p>
+                <p>
                   <strong>Lawcubator Technologies Pvt Ltd</strong>
-                </tr>
-                <tr>
+                </p>
+                <p>
                   758,3rd floor, 19th main, Dollar Scheme layout,HSR Layout 2nd
                   Sector, Bangalore, India-560102
-                </tr>
-                <tr>
+                </p>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={3}>
+                <p>
                   The above statements are true to the best of my knowledge and
                   belief
-                </tr>
-                <tr>
-                  <td>Date</td>
-                  <td>02-Jan-2023</td>
-                  <td>.....</td>
-                </tr>
-                <tr>
-                  <td>Place</td>
-                  <td></td>
-                  <td>Signature & Designation</td>
-                </tr>
-                <tr>For the treasury Use Only</tr>
-                <tr>
-                  <td>
-                    Received Rupees (in Words) <br /> <span> Rupees Zero</span>
-                  </td>
-                  <td>
-                    Rupees (in figures) <br /> <span> 0.00</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Date of Entry:</td>
-                  <td>Chalan No. :</td>
-                </tr>
-                <tr>
-                  <tr>
-                    <td>...</td>
-                    <td>...</td>
-                    <td>...</td>
-                  </tr>
-                  <tr>
-                    <td>Treasurer</td>
-                    <td>Accountant</td>
-                    <td>Treasury Officer / Agent or Manager</td>
-                  </tr>
-                </tr>
-              </tfoot>
-            </table>
-          </td>
-        </tr>
+                </p>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "baseline",
+                  }}
+                >
+                  <div>
+                    <tr>
+                      <td>Date</td>
+                      <td>02-Jan-2023</td>
+                    </tr>
+                    <tr>
+                      <td>Place</td>
+                      <td>.....</td>
+                    </tr>
+                  </div>
+                  <div>
+                    <tr>....</tr>
+                    <tr>Signature & Designation</tr>
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>For the treasury Use Only</tr>
+            <tr>
+              <td width={"50%"}>
+                Received Rupees (in Words) <br /> <span> Rupees Zero</span>
+              </td>
+              <td colSpan={2} width={"50%"}>
+                Rupees (in figures) <br /> <span> 0.00</span>
+              </td>
+            </tr>
+            <tr>
+              <td>Date of Entry:</td>
+              <td>Chalan No. :</td>
+            </tr>
+            <tr>
+              <td colSpan={3}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignContent: "center",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <p>....</p>
+                    <p>....</p>
+                    <p>....</p>
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                    }}
+                  >
+                    <p>Treasurer</p>
+                    <p>Accountant</p>
+                    <p>Treasury Officer / Agent or Manager</p>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </div>
+        </div>
       </table>
     </>
   );
